eea90e44e0e4e7354f411e8bb188b97eeac28a21
Charlie <charlie@archisketch.com>
2025-10-24 18:34:10 +0900

    feat: Project Migration

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,14 +1,16 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20250224_000001_create_structures_tables;
+mod m20251023_070251_create_project_table;
 
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(
-            m20250224_000001_create_structures_tables::Migration,
-        )]
+        vec![
+            Box::new(m20250224_000001_create_structures_tables::Migration),
+            Box::new(m20251023_070251_create_project_table::Migration),
+        ]
     }
 }
---
+++ migration/src/m20251023_070251_create_project_table.rs
@@ -0,0 +1,82 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(Project::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(Project::Id)
+                            .string()
+                            .not_null()
+                            .primary_key(),
+                    )
+                    .col(
+                        ColumnDef::new(Project::State)
+                            .string()
+                            .not_null()
+                            .default("ACTIVATED"),
+                    )
+                    .col(
+                        ColumnDef::new(Project::Name)
+                            .string()
+                            .not_null(),
+                    )
+                    .col(
+                        ColumnDef::new(Project::CoverImage)
+                            .string()
+                            .not_null()
+                    )
+                    .col(ColumnDef::new(Project::DefaultCoverImage).string())
+                    .col(
+                        ColumnDef::new(Project::FloorplanPath)
+                            .string()
+                            .not_null(),
+                    )
+                    .col(
+                        ColumnDef::new(Project::CreatedAt)
+                            .timestamp()
+                            .not_null()
+                            .default(Expr::current_timestamp()),
+                    )
+                    .col(
+                        ColumnDef::new(Project::UpdatedAt)
+                            .timestamp()
+                            .not_null()
+                            .default(Expr::current_timestamp()),
+                    )
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(
+                Table::drop()
+                    .table(Project::Table)
+                    .if_exists()
+                    .to_owned(),
+            )
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+enum Project {
+    Table,
+    Id,
+    State,
+    Name,
+    CoverImage,
+    DefaultCoverImage,
+    FloorplanPath,
+    CreatedAt,
+    UpdatedAt,
+}
